d99323ce6d77df4f1c1afebbeaf65a1c059ffe7a
valentingouverneur <valentin.gouverneur@gmail.com>
2025-03-07 18:44:43 +0100

    Update Calendar.tsx

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -6,8 +6,6 @@ import ConfirmModal from '../components/ConfirmModal';
 import ExpiryNotifications from '../components/ExpiryNotifications';
 import { NotificationService } from '../services/NotificationService';
 import { Product } from '../types/Product';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPen, faTrash, faBell } from '@fortawesome/free-solid-svg-icons';
 
 const Calendar: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
@@ -156,7 +154,9 @@ const Calendar: React.FC = () => {
                               className="p-2 text-gray-400 hover:text-blue-500 transition-colors"
                               title="Modifier le produit"
                             >
-                              <FontAwesomeIcon icon={faPen} className="w-5 h-5" />
+                              <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                              </svg>
                             </Link>
                             <button
                               onClick={() => {
@@ -166,7 +166,9 @@ const Calendar: React.FC = () => {
                               className="p-2 text-gray-400 hover:text-red-500 transition-colors"
                               title="Supprimer le produit"
                             >
-                              <FontAwesomeIcon icon={faTrash} className="w-5 h-5" />
+                              <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                              </svg>
                             </button>
                           </div>
                         </div>
